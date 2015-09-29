845dd94721fad7e2da9d0263904996fd4b5a13a1
Neil Volungis <nvolungis@gmail.com>
2015-09-28 23:18:59 -0400

    set chunk size

--- assets/javascripts/uploader.js
+++ assets/javascripts/uploader.js
@@ -223,10 +223,15 @@ var DropZone = React.createClass({
 
 
 var data = [];
-
+var flow = new Flow({
+  target: 'http://104.131.165.106/flow',
+  testChunks: false,
+  simultaneousUploads: 1,
+  method: 'octet',
+  flowChunkSize: 2048000
+});
 
 React.render(
-  <Uploader flow={new Flow({target: 'http://104.131.165.106/flow', testChunks: false, simultaneousUploads: 1, method: 'octet'})}/>,
-
+  <Uploader flow={flow} />,
   document.getElementById('content')
 );
